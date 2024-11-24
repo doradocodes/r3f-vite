120c03230793d7eb2aa97afa64735315a0d2bf6a
Dora Do <doradocodes@gmail.com>
2024-11-24 14:41:47 -0500

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/r3f-vite/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true, // also necessary
+  }
 })
